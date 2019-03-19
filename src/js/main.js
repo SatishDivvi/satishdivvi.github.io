6eb681547faa3bf642d7fc9a241dac2231d8b45f
divvisatish <divvinagavenkata.satish69@gmail.com>
2019-03-18 19:53:22 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('.slider').slick({
     infinite: true,
     slidesToShow: 1,
-    slidesToScroll:1
+    slidesToScroll:1,
+    autoplay: true,
+    autoplaySpeed: 1000
   });
 });
